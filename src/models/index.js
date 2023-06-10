8b61b94cf6bc96643525faf9fc6e856c5531122d
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-06-09 23:12:17 -0300

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+const Genre = require("./Genre");
+const Movie = require("./Movie");
+
+Movie.belongsToMany(Genre,{through:'MoviesGenres'})
+Genre.belongsToMany(Movie,{through:'MoviesGenres'})
---
+++ src/routes/movie.router.js
@@ -0,0 +1,19 @@
+const { getAll, create, getOne, remove, update, setGenres } = require('../controllers/movie.controllers');
+const express = require('express');
+
+const routerMovie = express.Router();
+
+routerMovie.route('/')
+    .get(getAll)
+    .post(create);
+
+routerMovie.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+ 
+    // /movie/:id/genres
+routerMovie.route('/:id/genres')
+    .post(setGenres)
+
+module.exports = routerMovie;
